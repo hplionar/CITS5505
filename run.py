d815d0c5794064643188cf90cfa69f2a18a29ecd
hplionar <24661999@student.uwa.edu.au>
2026-04-29 16:33:15 +0800

    Restore run.py

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-from app import create_app, db
-from app.models import User
+from app import create_app
 
 app = create_app()
-
-with app.app_context():
-    db.create_all()
-
-    if db.session.get(User, 1) is None:
-        user = User(id=1, username="Demo User")
-        db.session.add(user)
-        db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
