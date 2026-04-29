5d4b818fb2fb6251f85497433cadc3bda7e3db97
hplionar <24661999@student.uwa.edu.au>
2026-04-29 16:28:07 +0800

    Fixed base layout and added temporary dark-themed styling for StudyBuddy page

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+    if db.session.get(User, 1) is None:
+        user = User(id=1, username="Demo User")
+        db.session.add(user)
+        db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
