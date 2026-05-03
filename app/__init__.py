ab9439e99abb813370cb465c6702f5f0514e7a46
hplionar <24661999@student.uwa.edu.au>
2026-05-03 12:11:17 +0800

    Add database migration support

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
-def create_app():
+def create_app(config_object=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.routes import main
     app.register_blueprint(main)
